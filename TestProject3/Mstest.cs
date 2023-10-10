fe10a288f42f5effc5655bd1c7bc462fd7613254
ChubatovaTiger <n.chubatova@gmail.com>
2023-10-10 23:22:58 +0400

    check20tests

--- TestProject3/Mstest.cs
+++ TestProject3/Mstest.cs
@@ -3,10 +3,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TestProject3
 {
     [TestClass]
-    public class UnitTest1_pr3
+		public class UnitTestClass_0
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod01()
+        {
+			Assert.AreEqual(1,2);
+        }
+			[TestMethod]
+        public void TestMethod02()
+        {
+			Assert.AreEqual(1,1);
+        }
+		[TestMethod]
+        public void TestMethod03()
         {
 			Assert.AreEqual(1,2);
         }
@@ -14,7 +24,7 @@ namespace TestProject3
         [DataTestMethod]
         [DataRow( "Aa", "gb")]
         [DataRow( "Xx", "Yfhkdf")]
-        public void MSTestDataRowTestMethod2(string str1, string str2)
+        public void MSTestDataRowTestMethod0data(string str1, string str2)
         {
         }
     }
